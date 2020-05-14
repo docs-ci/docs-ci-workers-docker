ac84a30a54d746e3fd6c4329ec6719910ba98b7b
Brian Van Klaveren <bvan@slac.stanford.edu>
2020-05-14 14:49:10 -0700

    Add missing rootfs

---
+++ centos8/rootfs/var/jenkins_home/centos8/swarm-client.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z $JENKINS_MASTER_URL ]; then
+    JENKINS_MASTER_URL=https://lsst-docs-ci.ncsa.illinois.edu/jenkins/
+fi
+
+if [ -z $JENKINS_SWARM_LABEL ]; then
+    JENKINS_SWARM_LABEL=swarm
+fi
+
+if [ -z $JENKINS_SWARM_EXECUTORS ]; then
+    JENKINS_SWARM_EXECUTORS=1
+fi
+
+java -jar ${SWARM_HOME}/swarm-client.jar -disableSslVerification -executors ${JENKINS_SWARM_EXECUTORS} -master ${JENKINS_MASTER_URL} -labels ${JENKINS_SWARM_LABEL} -labels ${HOSTNAME} -name ${HOSTNAME} $*
