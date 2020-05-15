ce1a1caa81a3d59969d20b588c1323d480cfc0f2
Gabriele Comoretto <gcomoretto@lsst.org>
2020-05-14 17:08:51 -0700

    Fixes and updates

--- centos8/rootfs/var/jenkins_swarm/centos8/swarm-client.sh
+++ centos8/rootfs/var/jenkins_swarm/centos8/swarm-client.sh
@@ -5,11 +5,17 @@ if [ -z $JENKINS_MASTER_URL ]; then
 fi
 
 if [ -z $JENKINS_SWARM_LABEL ]; then
-    JENKINS_SWARM_LABEL=swarm
+    JENKINS_SWARM_LABEL=centos8-worker
 fi
 
 if [ -z $JENKINS_SWARM_EXECUTORS ]; then
     JENKINS_SWARM_EXECUTORS=1
 fi
 
-java -jar ${SWARM_HOME}/swarm-client.jar -disableSslVerification -executors ${JENKINS_SWARM_EXECUTORS} -master ${JENKINS_MASTER_URL} -labels ${JENKINS_SWARM_LABEL} -labels ${HOSTNAME} -name ${HOSTNAME} $*
+java -jar ${SWARM_HOME}/swarm-client.jar \
+  -disableSslVerification \
+  -executors ${JENKINS_SWARM_EXECUTORS} \
+  -master ${JENKINS_MASTER_URL} \
+  -name ${JENKINS_SWARM_LABEL} \
+  -username swarm \
+  -password ${SWARM_PWD} 2>&1 > ${SWARM_HOME}/swarm-client.log
