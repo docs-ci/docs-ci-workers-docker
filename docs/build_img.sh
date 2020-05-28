4b7e86c35a972519558fd2f98516a47ee957e6e1
Gabriele Comoretto <gcomoretto@lsst.org>
2020-05-28 15:52:06 -0700

    Added docugen docker image definition

---
+++ docs/build_img.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t docsci/docugen-worker:0.1.1 .
---
+++ docs/rootfs/var/jenkins_swarm/docugen/swarm-client.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z $JENKINS_MASTER_URL ]; then
+    JENKINS_MASTER_URL=https://lsst-docs-ci.ncsa.illinois.edu/jenkins/
+fi
+
+if [ -z $JENKINS_SWARM_LABEL ]; then
+    JENKINS_SWARM_LABEL=docugen
+fi
+
+if [ -z $JENKINS_SWARM_NAME ]; then
+    JENKINS_SWARM_NAME=${JENKINS_SWARM_LABEL}
+fi
+
+if [ -z $JENKINS_SWARM_EXECUTORS ]; then
+    JENKINS_SWARM_EXECUTORS=1
+fi
+
+java -jar ${SWARM_HOME}/swarm-client.jar \
+  -deleteExistingClients \
+  -disableSslVerification \
+  -executors ${JENKINS_SWARM_EXECUTORS} \
+  -master ${JENKINS_MASTER_URL} \
+  -name ${JENKINS_SWARM_NAME} \
+  -labels ${JENKINS_SWARM_LABEL} \
+  -tunnel 127.0.0.1:50000 \
+  -fsroot "/var/jenkins_home/${JENKINS_SWARM_NAME}" \
+  -username swarm \
+  -password ${SWARM_PWD} 2>&1 > ${SWARM_HOME}/swarm-client.log
