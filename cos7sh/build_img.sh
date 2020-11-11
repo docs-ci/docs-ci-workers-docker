e1575938959ed2474b36078f53c0a84886ebe85e
Gabriele Comoretto <gcomoretto@lsst.org>
2020-11-11 13:35:31 -0700

    Added cos7sh Docker definition

---
+++ cos7sh/build_img.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t docsci/cos7sh-worker:0.0.1 .
---
+++ cos7sh/rootfs/var/jenkins_swarm/cos7sh/swarm-client.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# configuring and running the Jenkins Swarm Worker
+if [ -z $JENKINS_MASTER_URL ]; then
+    JENKINS_MASTER_URL=https://lsst-docs-ci.ncsa.illinois.edu/jenkins/
+fi
+
+if [ -z $JENKINS_SWARM_LABEL ]; then
+    JENKINS_SWARM_LABEL="cos7sh"
+fi
+
+if [ -z $JENKINS_SWARM_NAME ]; then
+    JENKINS_SWARM_NAME=${JENKINS_SWARM_LABEL}
+fi
+
+if [ -z $JENKINS_SWARM_EXECUTORS ]; then
+    JENKINS_SWARM_EXECUTORS=1
+fi
+
+#export JSW_HOME="/var/jenkins_home/${JENKINS_SWARM_NAME}"
+
+# make available lsstsw (if not deployed)
+#if [ ! -f "${${JSW_HOME}}.deployed" ]; then
+  ${SWARM_HOME}/deploy_lsstsw.sh 2>&1 > ${SWARM_HOME}/deploy_lsstsw.log
+#else
+  # the Jenkins master may take some time before being up and running
+  sleep 30
+#fi
+
+java -jar ${SWARM_HOME}/swarm-client.jar \
+  -deleteExistingClients \
+  -disableSslVerification \
+  -executors ${JENKINS_SWARM_EXECUTORS} \
+  -master ${JENKINS_MASTER_URL} \
+  -name ${JENKINS_SWARM_NAME} \
+  -labels ${JENKINS_SWARM_LABEL} \
+  -tunnel 127.0.0.1:50000 \
+  -fsroot "${JSW_HOME}" \
+  -username swarm \
+  -password ${SWARM_PWD} 2>&1 > ${SWARM_HOME}/swarm-client.log
+
