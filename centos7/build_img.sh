5b5105c4d2f127c7a330f56059a0e1f5bf8c0fa3
Gabriele Comoretto <gcomoretto@lsst.org>
2020-11-17 21:00:07 +0000

    Improved docer image to be parametrized

--- centos7/build_img.sh
+++ centos7/build_img.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t docsci/centos7-worker:0.1.12 .
+docker build -t docsci/centos7-worker:0.2.1 .
--- centos7/rootfs/var/jenkins_swarm/centos7/deploy_lsstsw.sh
+++ centos7/rootfs/var/jenkins_swarm/centos7/deploy_lsstsw.sh
@@ -2,22 +2,26 @@
 #
 #
 
-cd ${JSW_HOME}
+if [ -z $LSSTSW_BRANCH ]; then
+    LSSTSW_BRANCH="master"
+fi
+
+cd ${HOME}
 if [ -d "lsstsw" ]; then
-  echo "Update existing ${JSW_HOME}/lsstsw"
+  echo "Update existing ${HOME}/lsstsw"
   cd lsstsw
   git pull
 else
-  echo "Deploy lsst in ${JSW_HOME}"
+  echo "Deploy lsst in ${HOME}"
   git clone https://github.com/lsst/lsstsw
   cd lsstsw
   git config user.email "docs-ci@lsst.org"
   git config user.name "Docs CI"
-  git checkout tickets/DM-25413
+  git checkout ${LSSTSW_BRANCH}
+  #git checkout tickets/DM-25413
   # when the node is first created, it has to deploy the infrastructure
   # not the parameter -P is used, to not install any new miniconda
   ./bin/deploy -P
-  touch .deployed
 fi
 
 git status
